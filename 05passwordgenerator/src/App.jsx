ed752cb90f4b65d6d598bff87f2440ca587ddf60
Deon-dev <kwabenagideonagu@gmail.com>
2025-10-11 02:11:16 +0100

    Using useCallback and useEffect

--- 05passwordgenerator/src/App.jsx
+++ 05passwordgenerator/src/App.jsx
@@ -1,12 +1,39 @@
-import { useState } from 'react'
+import { useEffect, useState, useCallback, useRef} from 'react'
 
 import './App.css'
+// import { useCallback } from 'react'; 
 
 function App() {
-  const [length, setLength] = useState(8)
-  const [numberAllowed, setNumberAllowed] = useState(false)
-  const [charAllowed, setCharAllowed] = useState(false)
-  const [password, setPassword] = useState('')
+  const [length, setLength] = useState(8);
+  const [numberAllowed, setNumberAllowed] = useState(false);
+  const [charAllowed, setCharAllowed] = useState(false);
+  const [password, setPassword] = useState('');
+
+  const passwordRef = useRef (null)
+
+  const generatePassword = useCallback(() => {
+    let pass = "";
+    let str = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+
+    if(numberAllowed) str += "0123456789"
+    if(charAllowed) str += "!@#$%^&*()_+"
+
+    for (let i = 1; i < length; i++) {
+      const char = Math.floor(Math.random() * str.length + 1);
+      pass += str.charAt(char);
+    }
+
+    setPassword(pass)
+  }, [length, numberAllowed, charAllowed])
+
+  const copyPasswordToCliboard = () => {
+    window.navigator.clipboard.writeText(password);
+    passwordRef.current?.select()
+  }
+
+  useEffect(() => {
+    generatePassword()
+  })
 
   return (
     <div className='w-full max-w-md mx-auto shadow-md rounded-lg px-4 py-3 my-8 bg-gray-800 text-orange-500'>
@@ -17,8 +44,10 @@ function App() {
         value={password} 
         className='outline-none w-full py-2 px-4 bg-white' 
         placeholder='Password' 
-        readOnly />
-        <button className='outline-none bg-blue-700 text-white px-3 py-0.5 shrink-0'>Copy</button>
+        readOnly
+        ref={passwordRef} 
+        />
+        <button className='outline-none bg-blue-700 text-white px-3 py-0.5 shrink-0' onClick={copyPasswordToCliboard}>Copy</button>
       </div>
       <div className='flex text-sm gap-x-2'>
         <div className="flex items-center gap-x-1">
