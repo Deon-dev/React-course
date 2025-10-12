c6df193febd79fae18e0f840044be08c7c50f049
Deon-dev <kwabenagideonagu@gmail.com>
2025-10-12 21:59:12 +0100

    creating a curency converter

---
+++ 06currencyConverter/src/App.jsx
@@ -0,0 +1,18 @@
+// import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  // const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div className='w-full h-screen flex flex-wrap justify-center items-center bg-cover bg-no-repeat' style={{backgroundImage: `url(https://images.unsplash.com/photo-1553729459-efe14ef6055d?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&q=80&w=2070)`}}>
+        <h1 className='bg-red-200'>Tailwind Class</h1>
+      </div>
+    </>
+  )
+}
+
+export default App
